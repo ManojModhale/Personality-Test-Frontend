9652be3a5afa43bf90bd3d112538a9dc4db4f659
BhagyashriKatkamwar <katkamwarbhagyashree@gmail.com>
2023-12-27 11:08:43 +0530

    Register Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './WelcomePage/welcome.component';
+import { RegisterComponent } from './RegistrationPage/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
