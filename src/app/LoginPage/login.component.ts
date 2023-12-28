3a59ed0a1540f39e85c0e23a98f9bafb5a619a99
ManojModhale <shreemodhale0102@gmail.com>
2023-12-28 17:24:18 +0530

    local Commit

--- src/app/LoginPage/login.component.ts
+++ src/app/LoginPage/login.component.ts
@@ -8,11 +8,10 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class LoginComponent {
 
-
-
   loginForm=new FormGroup({
     username:new FormControl("",Validators.required),
     password:new FormControl("",[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&]).{4,20}$/)  ]),
+    
   });
 
   
