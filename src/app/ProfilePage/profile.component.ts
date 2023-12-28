feb3a5b5565bd435b1dd0f234ab76c4fda62af96
BhagyashriKatkamwar <katkamwarbhagyashree@gmail.com>
2023-12-28 16:50:31 +0530

    local commit

---
+++ src/app/ProfilePage/profile.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+  fName: string = 'Bhagyashri'; // Initialize these variables with your data
+   lName: string = 'katkamwar';
+   email: string = 'bhagya@gmail.com';
+   cNumber: string = '8830152896';
+ 
+
+  
+
+
+
+
+  
+  
+}
--- src/app/RegistrationPage/register.component.ts
+++ src/app/RegistrationPage/register.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
+
 export class RegisterComponent {
 
+  
+  
 }
+  
+
+
+
+
+
+
