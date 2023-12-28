40005c03ca50a68aac6baadbc43a171a4a1a7b4d
ManojModhale <shreemodhale0102@gmail.com>
2023-12-28 14:09:32 +0530

    local Commit

--- src/app/LoginPage/login.component.ts
+++ src/app/LoginPage/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,25 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+
+
+  loginForm=new FormGroup({
+    username:new FormControl("",Validators.required),
+    password:new FormControl("",[Validators.required,Validators.minLength(8),Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&]).{8,}$/)  ]),
+  });
+
+  
+
+  get username():any{
+    return this.loginForm.get('username');
+  }
+
+  get password():any{
+    return this.loginForm.get('password')
+  }
+
+ 
+  onLogin(){
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './WelcomePage/welcome.component';
-<<<<<<< HEAD
 import { LoginComponent } from './LoginPage/login.component';
-=======
 import { RegisterComponent } from './RegistrationPage/register.component';
->>>>>>> 9652be3a5afa43bf90bd3d112538a9dc4db4f659
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ChangePasswordComponent } from './ForgotPassword/change-password.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-<<<<<<< HEAD
-    LoginComponent
-=======
-    RegisterComponent
->>>>>>> 9652be3a5afa43bf90bd3d112538a9dc4db4f659
+    LoginComponent,
+    RegisterComponent,
+    ChangePasswordComponent
   ],
   imports: [
     BrowserModule,
