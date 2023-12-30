e5da1c5c366ad8e4efeae4bcb253405ad9714495
Faiz2111 <hfaiz2111@gmail.com>
2023-12-30 16:57:11 +0530

    local commit

---
+++ src/app/TestingF/test-f.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test-f',
+  templateUrl: './test-f.component.html',
+  styleUrl: './test-f.component.css'
+})
+export class TestFComponent {
+
+}
